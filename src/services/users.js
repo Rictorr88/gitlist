346518aa5dce861b4555bdd5018b88a58e05ade1
Daniel Lopez <neo_19_8@hotmail.com>
2023-11-07 19:17:41 -0600

    class27

--- src/services/users.js
+++ src/services/users.js
@@ -3,24 +3,24 @@
 const BASE_API = 'https://api.github.com'
 
 async function fetchWrapper(url, options) {
-    const response = await fetch(url, options)
-    if (!response.ok) {
-        return {
-        data: null,
-        isError: true
-        }
-    }
-    const data = await response.json() 
+  const response = await fetch(url, options)
+  if (!response.ok) {
     return {
-        data,
-        isError: false
+      data: null,
+      isError: true
     }
+  }
+  const data = await response.json()
+  return {
+    data,
+    isError: false
+  }
 }
 
 export async function getUser(username) {
-    return fetchWrapper(fetch`${BASE_API}/users/${username}`)
+  return fetchWrapper(`${BASE_API}/users/${username}`)
 }
 
 export async function getRepos(username) {
-    return fetchWrapper(`${BASE_API}/users/${username}/repos`)
-} 
+  return fetchWrapper(`${BASE_API}/users/${username}/repos`)
+}
